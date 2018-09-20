a5c6c8ef95c8fbac4e81873575f4645cb7363bd5
Junhui Cao <lycaojh@gmail.com>
2018-09-20 21:56:29 +0800

    init project

---
+++ cplusplus/cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by junhui cao on 20/09/2018.
+//  Copyright © 2018 junhui cao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
